9e49f4cf9cea66ef52250d3f7d3f3a0a10ed35d4
Alfred Young <alfred@codenaturally.com>
2017-11-11 11:01:54 -0800

    p5 defs started

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import * as _ from 'lodash';
 import * as p5 from 'p5';
 
-const sketch = new p5(function (sketch: any) {
+const sketch = new p5(function (sketch: p5Sketch) {
 
     sketch.setup = function () {
 
     }
 
     sketch.draw = function () {
-
     }
 
 });
